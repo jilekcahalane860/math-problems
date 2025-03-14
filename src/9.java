37135df7c840dc78e1213d96e6d8d7a8354fc788
jilekcahalane860 <jilekcahalane860@gmail.com>
2025-03-14 18:39:41 +0000

    Add 9.java

---
+++ src/9.java
@@ -0,0 +1,14 @@
+  import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Code{
+public static void main (String[] args) {
+int i;
+for(i=0;i<100;i++){
+if(Math.random()>0.5){
+System.out.println("hello");
+}else{
+System.out.println("world");
+}}
+}}
