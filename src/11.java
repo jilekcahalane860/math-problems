53e0fdb7e482f3ab1d06f5b4b788ee83792ac98c
jilekcahalane860 <jilekcahalane860@gmail.com>
2025-03-16 06:25:12 +0000

    Add 11.java

---
+++ src/11.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main{
+
+    public static void main(String[] args){
+        int num = (int)(Math.random()*100)+1;
+        System.out.println(num);
+    }
+}
