67b8339c368bee67e58b88a42413c7d1cdb84e36
jilekcahalane860 <jilekcahalane860@gmail.com>
2025-03-23 16:42:23 +0000

    Add 17.java

---
+++ src/17.java
@@ -0,0 +1,7 @@
+public class MathProblemSolver {
+    public static void main(String[] args) {
+        // Example code for solving some basic math problems
+        System.out.println("Sqrt(16) = " + Math.sqrt(16));  // sqrt(16) should be 4
+        System.out.println("SquareRoot(1024) = " + Math.sqrt(1024));  // square root of 1024 should be 32
+    }
+}
