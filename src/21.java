1987d21155f8c7a9c0a6ad1f6b4bda8b84efdbe2
jilekcahalane860 <jilekcahalane860@gmail.com>
2025-03-31 16:24:44 +0000

    Add 21.java

---
+++ src/21.java
@@ -0,0 +1,11 @@
+public class MathProblems {
+    // Example code 1
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+    // Example code 2
+    static int sum(int a, int b) {
+        return a + b;
+    }
+}
