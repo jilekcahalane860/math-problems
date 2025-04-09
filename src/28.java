e647fba882b3a094b4acc2201fd5a678dfc24741
jilekcahalane860 <jilekcahalane860@gmail.com>
2025-04-09 15:38:19 +0000

    Add 28.java

---
+++ src/28.java
@@ -0,0 +1,19 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        // Example 1: Simple addition problem
+        int sum = 5 + 3;
+        System.out.println("The sum is: " + sum);
+
+        // Example 2: Multiplication problem
+        int product = 4 * 6;
+        System.out.println("The product is: " + product);
+
+        // Example 3: Division problem
+        double division = 10 / 2;
+        System.out.println("The result of the division is: " + division);
+
+        // Example 4: Floor division
+        long floorDivision = 9 / 3; // Note: in Java, floor division is implemented as Long.div(9, 3)
+        System.out.println("The result of floor division is: " + floorDivision);
+    }
+}
