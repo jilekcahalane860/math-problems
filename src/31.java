c1c3ebc496c5ed755f0e15c548f3ae7426fea819
jilekcahalane860 <jilekcahalane860@gmail.com>
2025-04-24 22:33:48 +0000

    Add 31.java

---
+++ src/31.java
@@ -0,0 +1,10 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        // Example Java code to demonstrate mathematical operations and loops.
+        double number1 = 5.0;
+        double number2 = 3.0;
+        double result = number1 + number2;
+
+        System.out.println("The sum of " + number1 + " and " + number2 + " is: " + result);
+    }
+}
