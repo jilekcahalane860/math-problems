c352ce34624856f6a004c7c5f193b3f18c9900eb
jilekcahalane860 <jilekcahalane860@gmail.com>
2025-04-25 00:24:11 +0000

    Add 32.java

---
+++ src/32.java
@@ -0,0 +1,7 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        // Example Java code for solving math-problems
+
+        System.out.println("Hello, World!");
+    }
+}
