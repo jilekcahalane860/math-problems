56f1a85f46a76e33f6d017158df5505384cae638
jilekcahalane860 <jilekcahalane860@gmail.com>
2025-04-26 05:38:26 +0000

    Add 33.java

---
+++ src/33.java
@@ -0,0 +1,7 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        // Code to perform mathematical operations
+        double result = 5 + 3;
+        System.out.println("The sum of 5 and 3 is: " + result);
+    }
+}
