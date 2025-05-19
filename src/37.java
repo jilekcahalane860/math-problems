22405777993112cbce19b4303dd4d446d7a98254
jilekcahalane860 <jilekcahalane860@gmail.com>
2025-05-19 04:19:20 +0000

    Add 37.java

---
+++ src/37.java
@@ -0,0 +1,8 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        int number = 5;
+        System.out.println("The sum of " + number + " and 3 is " + (number + 3));
+        double squareRoot = Math.sqrt(25);
+        System.out.println("The square root of 25 is " + squareRoot);
+    }
+}
